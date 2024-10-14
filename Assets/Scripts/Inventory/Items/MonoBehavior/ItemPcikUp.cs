d7319271c0b3c4c1605732f84f2296c37130b172
506817436 <506817436@qq.com>
2024-10-15 05:43:57 +0800

    ObjectScript

--- Assets/Scripts/Inventory/Items/MonoBehavior/ItemPcikUp.cs
+++ Assets/Scripts/Inventory/Items/MonoBehavior/ItemPcikUp.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ItemPcikUp : MonoBehaviour
 {
+    public ItemData_SO itemData;
     // Start is called before the first frame update
     void Start()
     {
