083ae91b7325105f96db2b4aa9d6979db73e35e9
MorePP <yupengmao2@gmail.com>
2024-10-18 16:58:59 +0100

    Jump
    添加可禁用的跳跃功能，
    增大了锅炉的面积，
    将NavMeshAgent半径调小

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,7 +8,18 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent playerAgent; // 玩家角色的 NavMesh Agent
+    private OffMeshLink[] offMeshLinks; // 场景中所有的offMesh link
+    [SerializeField] private bool haveSpring = true; // TODO 玩家是否装备弹簧
     // Start is called before the first frame update
+
+    void HandleOffMeshLink()
+    {
+        // 在玩家通过offmeshlink后直接传送回原位
+        Vector3 originalPosition = playerAgent.transform.position;
+        playerAgent.CompleteOffMeshLink();
+        playerAgent.Warp(originalPosition);
+    }
+
     void Start()
     {
         playerAgent = GetComponent<NavMeshAgent>(); //获得NavMeshAgent组件
@@ -32,7 +43,16 @@ public class PlayerController : MonoBehaviour
                 {
                     hit.collider.GetComponent<InteractableObject>().OnClick(playerAgent);
                 }
-                
+            }
+        }
+
+        // 玩家未装备弹簧则自定义OffMeshLink行为
+        if (!haveSpring)
+        {
+            playerAgent.autoTraverseOffMeshLink = false;
+            if (playerAgent.isOnOffMeshLink)
+            {
+                HandleOffMeshLink();
             }
         }
     }
