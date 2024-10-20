1e923b357801a72a8936a85526b49a16ba2a01ae
ss-7e <60181968+ss-7e@users.noreply.github.com>
2024-10-21 04:07:42 +0800

    Gear
    齿轮功能逻辑

--- Assets/Scripts/Other/Tag.cs
+++ Assets/Scripts/Other/Tag.cs
@@ -8,4 +8,5 @@ public class Tag
     public const string GROUND = "Ground";
     public const string PLAYER = "Player";
     public const string INTERACTABLE = "Interactable";
+    public const string GEAR = "Gear";
 }
