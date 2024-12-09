f174fdfd10986dda6e9e927f8c15aab735c979f2
ss-7e <60181968+ss-7e@users.noreply.github.com>
2024-12-09 18:32:49 +0800

    UI Cursor basic version

---
+++ Assets/Scripts/UI/Cursor.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseCursorController : MonoBehaviour
+{
+    public Texture2D defaultCursor; 
+    public Texture2D clickCursor;    
+    
+    private PlayerInput playerInput;
+
+    private void Start()
+    {
+        playerInput = new PlayerInput();
+        playerInput.Enable();
+        SetCursor(defaultCursor);
+        playerInput.UI.Click.performed += ctx =>
+        {
+            Debug.Log("Pressed" + UnityEngine.InputSystem.Mouse.current.position.ReadValue());
+            SetCursor(clickCursor);
+        };
+        playerInput.UI.Release.performed += ctx =>
+        {
+            Debug.Log("Release" + UnityEngine.InputSystem.Mouse.current.position.ReadValue());
+            SetCursor(defaultCursor);
+        };
+    }
+
+    private void OnClick()
+    {
+        SetCursor(clickCursor);
+    }
+    private void SetCursor(Texture2D cursorTexture)
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+}
